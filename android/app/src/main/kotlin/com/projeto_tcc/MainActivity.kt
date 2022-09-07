e9d9155a3f41419facd6eb08cebf4f870183393c
William Gomes <williamdouglasgomes@hotmail.com>
2022-09-07 12:45:28 -0300

    Configurações ios

---
+++ android/app/src/main/kotlin/com/projeto_tcc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.projeto_tcc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
 import 'app/enums/enums.dart';
+import 'app/views/stylePages/app_colors.dart';
 import 'flavors.dart';
 
 void main() {
+  Map<int, Color> color = {
+    50: AppColors.purpleDefaultColor,
+    100: AppColors.purpleDefaultColor,
+    200: AppColors.purpleDefaultColor,
+    300: AppColors.purpleDefaultColor,
+    400: AppColors.purpleDefaultColor,
+    500: AppColors.purpleDefaultColor,
+    600: AppColors.purpleDefaultColor,
+    700: AppColors.purpleDefaultColor,
+    800: AppColors.purpleDefaultColor,
+    900: AppColors.purpleDefaultColor,
+  };
+  MaterialColor colorCustom = MaterialColor(0XFF62407E, color);
   F.appFlavor = Flavor.DEV;
-  runApp(App());
+  runApp(App(color: colorCustom));
 }
